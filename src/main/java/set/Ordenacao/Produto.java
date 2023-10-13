648550e7a9afc7246befc1c5e889db1f3bd26224
joaophellipe <joaophellipe100@gmail.com>
2023-10-13 16:58:03 -0300

    Adicionando Operações Basicas da interface Map
    Signed-off-by: joaophellipe <joaophellipe100@gmail.com>

---
+++ src/main/java/set/Ordenacao/Produto.java
@@ -0,0 +1,71 @@
+package main.java.set.Ordenacao;
+
+import main.java.list.Ordenacao.Pessoa;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+public class Produto implements Comparable<Produto> {
+    private String nome;
+    private long cod;
+    private double preco;
+    private int quantidade;
+
+    public Produto(long cod, String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.cod = cod;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public long getCod() {
+        return cod;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto produto)) return false;
+        return getCod() == produto.getCod();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCod());
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", cod=" + cod +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Produto p) {
+        return nome.compareToIgnoreCase(p.getNome());
+    }
+}
+
+class ComparatorPorPreco implements Comparator<Produto> {
+
+    @Override
+    public int compare(Produto p1, Produto p2) {
+        return Double.compare(p1.getPreco(), p2.getPreco());
+    }
+}
